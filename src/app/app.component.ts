88d21609d98561521e0684a4a4b141b0785188e6
pelayo <100432533@alumnos.uc3m.es>
2022-04-25 22:20:11 +0200

    Markers incluidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MapMarker, GoogleMap } from '@angular/google-maps';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+
+
+export class AppComponent implements OnInit{
   title = 'EntregableP2';
   center = {lat: 24, lng: 12};
   zoom = 15;
   display?: google.maps.LatLngLiteral;
 
-
   ngOnInit() {
+    
+    
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       }
+      // this.map = new google.maps.Map(document.getElementById('map'),{})
+
+
     })
   }
+  markers:  any[] = [];
+  // private map: google.maps.Map
+
+
+  addMarker() {
+    this.markers.push({
+      position: { lat: this.center.lat + (Math.random()) / 100,
+                  lng: this.center.lng + (Math.random() )/ 100},
+      label: {
+       color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+    })
+  }
+  
+
 }
