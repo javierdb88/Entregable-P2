d270818bab475dcd7128e372f7052961f55e6380
pelayo <100432533@alumnos.uc3m.es>
2022-04-25 10:46:13 +0200

    inicio+login

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  tipo_vehiculo:string = '';
+  /*marcasCoche:string[] = ["Volkswagen","BMW","Tesla","Mercedes","Mazda"];
+  marcasMotos:string[] = ["Suzuki","Yamaha",]*/
+  modelo:string = '';
+  tipos:string[] = ["Coche","Moto"];
+  marca:string='';
+
+}
